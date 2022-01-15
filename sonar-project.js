fc994a9684c6484da860520b27c13b6f6d35fa10
sunny00000000 <96020222+sunny00000000@users.noreply.github.com>
2022-01-15 07:20:46 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '5c2e067fbf0f29be84519fafc2be1dd94eab6c24',
+	    'sonar.login': '481b056bc14b6902d582fa1bfa0b3581dede148a',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
